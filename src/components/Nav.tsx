dbc62bafa0752b61c6e2a8e55be8b88273380a6d
NikhilMahalingam <mahalingamnikhil@gmail.com>
2024-01-22 20:36:09 -0500

    minor fix

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,7 +6,7 @@ const Nav = () => {
 
     const [navOpen, setNavOpen] = useState(false);
     const location = useLocation();
-    const isAboutPage = location.pathname === '/About' || location.pathname === '/Experience' || location.pathname === '/Projects' || location.pathname === '/';
+    const isAboutPage = location.pathname === '/About' || location.pathname === '/Experience' || location.pathname === '/Projects' || location.pathname === '/' || location.pathname === '/react-portfolio';
   
     return (
       <div className="nav">
